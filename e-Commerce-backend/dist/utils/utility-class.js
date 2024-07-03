e05cc8cd9c5c1f9c5948f98d4ec24081b8e7b085
Rishabh Srivastava <rishabh111202@gmail.com>
2024-07-03 15:54:16 +0530

    Delete e-Commerce-backend/dist directory

--- e-Commerce-backend/dist/utils/utility-class.js
+++
@@ -1,9 +0,0 @@
-class ErrorHandler extends Error {
-    constructor(message, statusCode) {
-        super(message);
-        this.message = message;
-        this.statusCode = statusCode;
-        this.statusCode = statusCode;
-    }
-}
-export default ErrorHandler;
